da815d49f48ba91e6a342232efd8dcff34189df5
MartinMirabelli <mirabellielec@gmail.com>
2023-04-15 13:31:11 -0300

    Commit inicial

--- app/src/main/java/com/example/comparadorpreciosmirabelli/MainActivity.kt
+++ app/src/main/java/com/example/comparadorpreciosmirabelli/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-    //comentario de martin
+    //comentario de martin a
 
 }
